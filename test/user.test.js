7ce3391e4365be8a90e4c831307beab2eff9dd30
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-05 17:19:37 +0700

    Create Get User API

--- test/user.test.js
+++ test/user.test.js
@@ -139,3 +139,23 @@ describe('POST /api/users/login', function () {
 	});
 });
 
+describe('GET /api/users/current', function () {
+	beforeEach(async () => {
+		await createTestUser();
+	});
+
+	afterEach(async () => {
+		await removeTestUser();
+	});
+
+	it('Should can get current user', async () => {
+		const result = await supertest(web)
+			.get('/api/users/current')
+			.set('Authorization', 'test');
+
+		expect(result.status).toBe(200);
+		expect(result.body.data.username).toBe('test');
+		expect(result.body.data.name).toBe('test');
+	})
+})
+
