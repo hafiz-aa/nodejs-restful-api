68aec5dff37bced46af84bfd2e0089e77f712c48
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-06 15:56:08 +0700

    Create Update User API

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,9 +1,10 @@
 import { validate } from '../validation/validation.js';
-import { getUserValidation, loginUserValidation, registerUserValidation } from '../validation/user-validation.js';
+import { getUserValidation, loginUserValidation, registerUserValidation, updateUserValidation } from '../validation/user-validation.js';
 import { prismaClient } from '../application/database.js';
 import { ResponseError } from '../error/response-error.js';
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
+import { request } from 'express';
 
 const register = async (request) => {
 	const user = validate(registerUserValidation, request);
@@ -85,6 +86,39 @@ const get = async (username) => {
 	return user;
 }
 
+const update = async (request) => {
+	const user = validate(updateUserValidation, request)
+
+	const totalUserInDatabase = await prismaClient.user.count({
+		where: {
+			username: user.username
+		}
+	});
+
+	if (totalUserInDatabase !== 1) {
+		throw new ResponseError(404, "User is not found");
+	}
+
+	const data = {};
+	if (user.name) {
+		data.name = user.name;
+	}
+	if (user.password) {
+		data.password = await bcrypt.hash(user.password, 10);
+	}
+
+	return prismaClient.user.update({
+		where: {
+			username: user.username,
+		},
+		data: data,
+		select: {
+			username: true,
+			name: true
+		}
+	})
+}
+
 export default {
-	register, login, get
+	register, login, get, update
 }
--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,7 @@
 import supertest from "supertest";
 import { web } from "../src/application/web.js";
 import { logger } from "../src/application/logging.js";
-import { createTestUser, removeTestUser } from "./test-util.js";
+import { createTestUser, getTestUser, removeTestUser } from "./test-util.js";
 import bcrypt from "bcrypt";
 
 describe('POST /api/users', function () {
@@ -165,6 +165,72 @@ describe('GET /api/users/current', function () {
 
 		expect(result.status).toBe(401);
 		expect(result.body.errors).toBeDefined();
-	})
+	});
+});
+
+describe('PATCH /api/users/current', function () {
+	beforeEach(async () => {
+		await createTestUser();
+	});
+
+	afterEach(async () => {
+		await removeTestUser();
+	});
+
+	it('Should can update user', async () => {
+		const result = await supertest(web)
+			.patch("/api/users/current")
+			.set("Authorization", "test")
+			.send({
+				name: "Nera",
+				password: "rahasialagi"
+			});
+
+		expect(result.status).toBe(200);
+		expect(result.body.data.username).toBe("test");
+		expect(result.body.data.name).toBe("Nera");
+
+		const user = await getTestUser();
+		expect(await bcrypt.compare("rahasialagi", user.password)).toBe(true);
+	});
+
+	it('Should can update user name', async () => {
+		const result = await supertest(web)
+			.patch("/api/users/current")
+			.set("Authorization", "test")
+			.send({
+				name: "Nera"
+			});
+
+		expect(result.status).toBe(200);
+		expect(result.body.data.username).toBe("test");
+		expect(result.body.data.name).toBe("Nera");
+	});
+
+	it('Should can update user password', async () => {
+		const result = await supertest(web)
+			.patch("/api/users/current")
+			.set("Authorization", "test")
+			.send({
+				password: "rahasialagi"
+			});
+
+		expect(result.status).toBe(200);
+		expect(result.body.data.username).toBe("test");
+		expect(result.body.data.name).toBe("test");
+
+		const user = await getTestUser();
+		expect(await bcrypt.compare("rahasialagi", user.password)).toBe(true);
+	});
+
+	it('Should reject if request is not valid', async () => {
+		const result = await supertest(web)
+			.patch("/api/users/current")
+			.set("Authorization", "salah")
+			.send({});
+
+		expect(result.status).toBe(401);
+	});
+
 })
 
