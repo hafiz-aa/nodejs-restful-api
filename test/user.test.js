7cadf47fdd460b064328232c4568307e05cedd0d
hafiz-aa <mhafiz.aa@gmail.com>
2023-07-06 17:17:10 +0700

    Create Logout User API

--- test/user.test.js
+++ test/user.test.js
@@ -231,6 +231,35 @@ describe('PATCH /api/users/current', function () {
 
 		expect(result.status).toBe(401);
 	});
+});
 
-})
+describe('DELETE api/users/logout', function () {
+	beforeEach(async () => {
+		await createTestUser();
+	});
+
+	afterEach(async () => {
+		await removeTestUser();
+	});
+
+	it('Should can logout', async () => {
+		const result = await supertest(web)
+			.delete('/api/users/logout')
+			.set('Authorization', 'test');
+
+		expect(result.status).toBe(200);
+		expect(result.body.data).toBe("OK");
+
+		const user = await getTestUser();
+		expect(user.token).toBeNull();
+	});
+
+	it('Should reject logout if token is invalid', async () => {
+		const result = await supertest(web)
+			.delete('/api/users/logout')
+			.set('Authorization', 'salah');
+
+		expect(result.status).toBe(401);
+	});
+});
 
